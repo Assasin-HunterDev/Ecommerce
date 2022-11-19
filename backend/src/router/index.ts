6e85d81e5fdc8dbe5365083510ce4b1f34aea5da
fvucemilo <fvucemilo@tvz.hr>
2022-11-19 17:23:41 +0100

    Create forgot password and reset password pages

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,11 +1,23 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Login from "../views/Login.vue";
+import ForgotPassword from "../views/ForgotPassword.vue";
+import ResetPassword from "../views/ResetPassword.vue";
 
 const routes: any = [
     {
         path: '/login',
         name: 'login',
         component: Login
+    },
+    {
+        path: '/forgot-password',
+        name: 'forgotPassword',
+        component: ForgotPassword
+    },
+    {
+        path: '/reset-password/:token',
+        name: 'resetPassword',
+        component: ResetPassword
     },
 ];
 
--- backend/src/views/ResetPassword.vue
+++ backend/src/views/ResetPassword.vue
@@ -1,36 +1,33 @@
 <template>
-    <GuestLayout title="Sign in to your account">
-        <form class="mt-8 space-y-6" method="POST">
+    <GuestLayout title="Reset your password">
+        <form class="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" value="true"/>
             <div class="-space-y-px rounded-md shadow-sm">
                 <div>
-                    <label for="email-address" class="sr-only">Email address</label>
-                    <input id="email-address" name="email" type="email" autocomplete="email" required=""
+                    <label for="new-password" class="sr-only">New Password</label>
+                    <input id="new-password" name="password" type="password" autocomplete="current-password"
+                           required=""
                            class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                           placeholder="Email address"/>
+                           placeholder="New Password"/>
                 </div>
                 <div>
-                    <label for="password" class="sr-only">Password</label>
-                    <input id="password" name="password" type="password" autocomplete="current-password" required=""
+                    <label for="password-repeat" class="sr-only">Repeat Password</label>
+                    <input id="password-repeat" name="password_repeat" type="password"
+                           autocomplete="current-password" required=""
                            class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                           placeholder="Password"/>
+                           placeholder="Repeat Password"/>
                 </div>
+
             </div>
 
             <div class="flex items-center justify-between">
-                <div class="flex items-center">
-                    <input id="remember-me" name="remember-me" type="checkbox"
-                           class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"/>
-                    <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
-                </div>
-
+                <div class="flex items-center"></div>
                 <div class="text-sm">
-                    <router-link :to="{name: 'forgotPassword'}"
-                                 class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?
+                    <router-link :to="{name: 'login'}" class="font-medium text-indigo-600 hover:text-indigo-500">
+                        Sign in
                     </router-link>
                 </div>
             </div>
-
             <div>
                 <button type="submit"
                         class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
@@ -38,7 +35,7 @@
                           <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
                                           aria-hidden="true"/>
                         </span>
-                    Sign in
+                    Submit
                 </button>
             </div>
         </form>
