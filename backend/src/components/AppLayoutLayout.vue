7d960824c8d4715cf6011f253508e3a448463fdb
Assasin-Hunterdev <noahthomson422@gmail.com>
2022-11-19 17:43:41 +0100

    Create layout for authorized users

---
+++ backend/src/components/AppLayoutLayout.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex min-h-full">
+        <!--    Sidebar    -->
+        <div class="w-[200px] bg-indigo-600">
+            Sidebar
+        </div>
+        <div>
+            <!--     Header       -->
+            <header class="h-8 shadow bg-gray-50">
+                Header
+            </header>
+            <!--    Content        -->
+            <main>
+                <router-view>
+                    Content
+                </router-view>
+            </main>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const {title} = defineProps({
+    title: String
+});
+</script>
+
+<style scoped>
+
+</style>
