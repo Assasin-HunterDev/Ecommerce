921a1e9c1831a3a037594b6676cb980d75b0082d
Assasin-Hunterdev <noahthomson422@gmail.com>
2022-11-19 17:19:44 +0100

    Create login form page

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,6 +1,13 @@
 import {createRouter, createWebHistory} from "vue-router";
+import Login from "../views/Login.vue";
 
-const routes: any = [];
+const routes: any = [
+    {
+        path: '/login',
+        name: 'login',
+        component: Login
+    },
+];
 
 const router = createRouter({
     history: createWebHistory(),
