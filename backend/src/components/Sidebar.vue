4d7e10c20d94b8fe0f825f7fb9eec94fdb5891ca
fvucemilo <fvucemilo@tvz.hr>
2022-11-19 18:59:43 +0100

    Create sidebar component

---
+++ backend/src/components/Sidebar.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="min-w-[160px] w-[160px] transition-all bg-indigo-600 text-white py-4 px-2">
+        <router-link to=""
+                     class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+      <span class="mr-2 text-gray-300">
+        <HomeIcon class="w-5"/>
+      </span>
+            <span class="text-xs">
+        Dashboard
+      </span>
+        </router-link>
+        <router-link to=""
+                     class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+      <span class="mr-2 text-gray-300">
+        <ViewListIcon class="w-5"/>
+      </span>
+            <span class="text-xs">
+        Products
+      </span>
+        </router-link>
+        <router-link to=""
+                     class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+      <span class="mr-2 text-gray-300">
+        <ViewListIcon class="w-5"/>
+      </span>
+            <span class="text-xs">
+        Orders
+      </span>
+        </router-link>
+        <router-link to=""
+                     class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+      <span class="mr-2 text-gray-300">
+        <UsersIcon class="w-5"/>
+      </span>
+            <span class="text-xs">
+        Users
+      </span>
+        </router-link>
+        <router-link to=""
+                     class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+      <span class="mr-2 text-gray-300">
+        <UserGroupIcon class="w-5"/>
+      </span>
+            <span class="text-xs">
+        Customers
+      </span>
+        </router-link>
+        <router-link to=""
+                     class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+      <span class="mr-2 text-gray-300">
+        <ChartBarIcon class="w-5"/>
+      </span>
+            <span class="text-xs">
+        Reports
+      </span>
+        </router-link>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {HomeIcon, UserGroupIcon, UsersIcon, ViewListIcon, ChartBarIcon} from '@heroicons/vue/outline'
+</script>
+
+<style scoped>
+
+</style>
