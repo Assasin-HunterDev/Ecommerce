aa6d02cf9b2abee1112f4ba84174bd1a81468e4c
Assasin-Hunterdev <noahthomson422@gmail.com>
2022-11-20 17:50:35 +0100

    Implement login and logout for admin in Laravel

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request): Response|Application|ResponseFactory
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required',
+            'remember' => 'boolean'
+        ]);
+        $remember = $credentials['remember'] ?? false;
+        unset($credentials['remember']);
+        if (!Auth::attempt($credentials, $remember)) {
+            return response([
+                'message' => 'Email or password is incorrect!'
+            ], 422);
+        }
+
+        /** @var User $user */
+        $user = Auth::user();
+        if (!$user->is_admin) {
+            Auth::logout();
+
+            return response([
+                'message' => 'You don\'t have permission to authenticate as admin'
+            ], 403);
+        }
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function logout(): Response|Application|ResponseFactory
+    {
+        /** @var User $user */
+        Auth::logout();
+        $user->currentAccessToken()->delete();
+
+        return response('', 204);
+    }
+}
