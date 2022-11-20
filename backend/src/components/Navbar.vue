98af6e52bf95b7cf97f0123ca51067edab6beb44
fvucemilo <fvucemilo@tvz.hr>
2022-11-21 00:58:06 +0100

    Show validation error messages in login form, add splash screen loader and output user in navbar

--- backend/src/components/Navbar.vue
+++ backend/src/components/Navbar.vue
@@ -7,53 +7,37 @@
         <Menu as="div" class="relative inline-block text-left">
             <MenuButton class="flex items-center">
                 <img src="https://randomuser.me/api/portraits/men/1.jpg" class="rounded-full w-8 mr-2">
-                <small>John Smith</small>
-                <ChevronDownIcon
-                    class="h-5 w-5 text-indigo-200 hover:text-indigo-100"
-                    aria-hidden="true"
-                />
+                <small>{{ currentUser.name }}</small>
+                <ChevronDownIcon aria-hidden="true" class="h-5 w-5 text-indigo-200 hover:text-indigo-100"/>
             </MenuButton>
 
-            <transition
-                enter-active-class="transition duration-100 ease-out"
-                enter-from-class="transform scale-95 opacity-0"
-                enter-to-class="transform scale-100 opacity-100"
-                leave-active-class="transition duration-75 ease-in"
-                leave-from-class="transform scale-100 opacity-100"
-                leave-to-class="transform scale-95 opacity-0"
-            >
+            <transition enter-active-class="transition duration-100 ease-out"
+                        enter-from-class="transform scale-95 opacity-0" enter-to-class="transform scale-100 opacity-100"
+                        leave-active-class="transition duration-75 ease-in"
+                        leave-from-class="transform scale-100 opacity-100"
+                        leave-to-class="transform scale-95 opacity-0">
                 <MenuItems
-                    class="absolute right-0 mt-2 w-36 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                >
+                    class="absolute right-0 mt-2 w-36 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <div class="px-1 py-1">
                         <MenuItem v-slot="{ active }">
-                            <button
-                                :class="[
-                  active ? 'bg-indigo-700 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
-                            >
-                                <UserIcon
-                                    :active="active"
-                                    :class="[active ? 'mr-2 h-5 w-5 text-white' : 'mr-2 h-5 w-5 text-indigo-700']"
-                                    aria-hidden="true"
-                                />
+                            <button :class="[
+                            active ? 'bg-indigo-700 text-white' : 'text-gray-900',
+                            'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                        ]">
+                                <UserIcon :active="active"
+                                          :class="[active ? 'mr-2 h-5 w-5 text-white' : 'mr-2 h-5 w-5 text-indigo-700']"
+                                          aria-hidden="true"/>
                                 Profile
                             </button>
                         </MenuItem>
                         <MenuItem v-slot="{ active }">
-                            <button
-                                @click="logout"
-                                :class="[
-                  active ? 'bg-indigo-700 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
-                            >
-                                <LogoutIcon
-                                    :active="active"
-                                    :class="[active ? 'mr-2 h-5 w-5 text-white' : 'mr-2 h-5 w-5 text-indigo-700']"
-                                    aria-hidden="true"
-                                />
+                            <button :class="[
+                            active ? 'bg-indigo-700 text-white' : 'text-gray-900',
+                            'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                        ]" @click="logout">
+                                <LogoutIcon :active="active"
+                                            :class="[active ? 'mr-2 h-5 w-5 text-white' : 'mr-2 h-5 w-5 text-indigo-700']"
+                                            aria-hidden="true"/>
                                 Logout
                             </button>
                         </MenuItem>
@@ -65,6 +49,7 @@
 </template>
 
 <script setup lang="ts">
+import {computed} from "vue";
 import {useRouter} from "vue-router";
 import store from "../store";
 import {LogoutIcon, MenuIcon, UserIcon} from "@heroicons/vue/outline";
@@ -75,6 +60,8 @@ const router = useRouter();
 
 const emit = defineEmits(["toggle-sidebar"]);
 
+const currentUser = computed(() => store.state.user.data);
+
 function logout() {
     store.dispatch("logout").then(() => {
         router.push({name: 'login'})
@@ -83,4 +70,5 @@ function logout() {
 </script>
 
 <style scoped>
+
 </style>
