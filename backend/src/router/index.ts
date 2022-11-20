8585633fdab0e4006832505ad54c2836666c185f
Assasin-Hunterdev <noahthomson422@gmail.com>
2022-11-20 20:19:15 +0100

    Create dashboard page

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from "vue-router";
 import store from "../store";
 import AppLayout from "../components/AppLayoutLayout.vue";
+import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
 import ForgotPassword from "../views/ForgotPassword.vue";
 import ResetPassword from "../views/ResetPassword.vue";
@@ -11,7 +12,13 @@ const routes: any = [
         path: "/app",
         name: "app",
         component: AppLayout,
-        children: [],
+        children: [
+            {
+                path: 'dashboard',
+                name: 'app.dashboard',
+                component: Dashboard
+            },
+        ],
         meta: {
             requiresAuth: true
         }
@@ -56,7 +63,7 @@ router.beforeEach((to, from, next) => {
     to.meta.requiresAuth && !store.state.user.token
         ? (next({name: "login"}))
         : to.meta.requiresGuest && store.state.user.token
-            ? (next({name: "app"}))
+            ? (next({name: "app.dashboard"}))
             : (next());
 });
 
--- backend/src/views/NotFound.vue
+++ backend/src/views/NotFound.vue
@@ -6,7 +6,7 @@
                 <h1 class="text-5xl font-bold mb-2">Page Not Found</h1>
                 <p class="text-gray-400  mb-10">Please check the url in address bar and try again</p>
                 <div>
-                    <router-link :to="{name: 'app'}"
+                    <router-link :to="{name: 'app.dashboard'}"
                                  class="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         Go back home
                     </router-link>
