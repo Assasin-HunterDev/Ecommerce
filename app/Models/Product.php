37412d27ef6c4e4049e03b70eb0e6ca85a98659c
fvucemilo <fvucemilo@tvz.hr>
2022-11-21 12:33:29 +0100

    Install and configure laravel-sluggable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Product extends Model
 {
     use HasFactory;
+    use HasSlug;
+    use SoftDeletes;
+
+    /**
+     * Get the options for generating the slug.
+     *
+     * @return SlugOptions
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 }
