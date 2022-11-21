587f6bfe8633740e24604645341200178a822f9a
fvucemilo <fvucemilo@tvz.hr>
2022-11-21 19:59:50 +0100

    Create product CRUD api controller, request and resource

---
+++ app/Http/Resources/ProductListResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Nette\Utils\DateTime;
+
+class ProductListResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'image' => $this->image,
+            'price' => $this->price,
+            'updated_at' => (new DateTime($this->updated_at))->format('Y-m-d H:i:s')
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     Route::get('/user', [AuthController::class, 'getUser']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('/product', ProductController::class);
 });
 
 Route::post('/login', [AuthController::class, 'login']);
