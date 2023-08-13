11d1b9a95e40023b4512ea65233cba36f36471c7
Assasin-Hunterdev <noahthomson422@gmail.com>
2023-08-13 11:06:32 +0200

    Create middleware guestOrVerified

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'guestOrVerified' => \App\Http\Middleware\GuestOrVerified::class,
 
         'admin' => \App\Http\Middleware\Admin::class
     ];
