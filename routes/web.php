8e22e1e38e943e51e42ad8be2df609a8471924e2
fvucemilo <fvucemilo@tvz.hr>
2023-08-13 11:06:32 +0200

    Create middleware guestOrVerified

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -15,14 +16,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ProductController::class, 'index'])->name('home');
-Route::get('/product/{product:slug}', [ProductController::class, 'view'])->name('product.view');
+Route::middleware(['guestOrVerified'])->group(function () {
+    Route::get('/', [ProductController::class, 'index'])->name('home');
+    Route::get('/product/{product:slug}', [ProductController::class, 'view'])->name('product.view');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    Route::prefix('/cart.')->name('cart.')->group(function () {
+        Route::get('/', [CartController::class, 'index'])->name('index');
+        Route::get('/add/{product:slug}', [CartController::class, 'add'])->name('add');
+        Route::get('/remove/{product:slug}', [CartController::class, 'remove'])->name('remove');
+        Route::get('/update-quantity/{product:slug}', [CartController::class, 'updateQuantity'])->name('update-quantity');
+    });
+});
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
