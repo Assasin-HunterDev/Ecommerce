2458d55df4d5b43b4bf802b27e0474ed5883a0db
Assasin-Hunterdev <noahthomson422@gmail.com>
2023-08-14 17:27:21 +0200

    Implement Password update

--- resources/views/profile/view.blade.php
+++ resources/views/profile/view.blade.php
@@ -280,6 +280,37 @@
                     <x-button class="w-full">Update</x-button>
                 </form>
             </div>
+            <div class="bg-white p-3 shadow rounded-lg">
+                <form action="{{route('profile_password.update')}}" method="POST">
+                    @csrf
+                    <h2 class="text-xl font-semibold mb-2">Update Password</h2>
+                    <div class="mb-3">
+                        <x-input
+                            type="password"
+                            name="old_password"
+                            placeholder="Your Current Password"
+                            class="w-full focus:border-purple-600 focus:ring-purple-600 border-gray-300 rounded"
+                        />
+                    </div>
+                    <div class="mb-3">
+                        <x-input
+                            type="password"
+                            name="new_password"
+                            placeholder="New Password"
+                            class="w-full focus:border-purple-600 focus:ring-purple-600 border-gray-300 rounded"
+                        />
+                    </div>
+                    <div class="mb-3">
+                        <x-input
+                            type="password"
+                            name="new_password_confirmation"
+                            placeholder="Repeat New Password"
+                            class="w-full focus:border-purple-600 focus:ring-purple-600 border-gray-300 rounded"
+                        />
+                    </div>
+                    <x-button>Update</x-button>
+                </form>
+            </div>
         </div>
     </div>
 </x-app-layout>
