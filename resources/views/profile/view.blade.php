6fa75558ec8052dde645d26ddb09f75541e6dd19
fvucemilo <fvucemilo@tvz.hr>
2023-08-14 15:00:17 +0200

    Create Profile Controller

---
+++ resources/views/profile/view.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    My Profile
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -29,9 +29,7 @@ Route::middleware(['guestOrVerified'])->group(function () {
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'view'])->name('profile');
 });
 
 require __DIR__.'/auth.php';
